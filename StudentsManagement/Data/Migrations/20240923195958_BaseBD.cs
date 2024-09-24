0596d89eeb1f6a99884741ac920dbe6fde9b0dce
henry hosken <henry_hosken12@outlook.com>
2024-09-23 21:46:26 -0300

    Add project files.

---
+++ StudentsManagement/Data/Migrations/20240923195958_BaseBD.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentsManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class BaseBD : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MiddleName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EmailAddres = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Addres = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
---
+++ StudentsManagement/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentsManagement.Models
+{
+    public class Student
+    {
+        [Key]
+
+        public required int Id { get; set; }
+        public required string? FirstName { get; set; }
+        public required string? MiddleName { get; set; }
+        public required string? LastName { get; set; }
+        public required string? EmailAddres { get; set; }
+        public required string? Addres { get; set; }
+        public required string? PhoneNumber { get; set; }
+        public required string? Country { get; set; }
+    }
+}
